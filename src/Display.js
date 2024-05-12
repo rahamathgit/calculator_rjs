45fc323fda09a37336fc2871dee808f7ace7f3bb
rahamathgit <lathief.rahamath@gmail.com>
2024-05-12 13:16:48 +0530

    first commit

---
+++ src/Display.js
@@ -0,0 +1,48 @@
+import React, { useState } from "react";
+
+const Display = () => {
+
+const [value, setValue] = useState('');
+    return(
+    <div className="container">
+        <div className="parent">
+            <form>
+                <div className="child">
+                    <input type="text" value={value} />
+                </div>
+                <div>
+                    <input type="button" value={"AC"} className="operator" onClick={e => setValue('')}/>
+                    <input type="button" value={"DE"} className="operator" onClick={e => setValue(value.slice(0, -1))}/>
+                    <input type="button" value={"."} className="operator" onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"/"} className="operator" onClick={e => setValue(value + e.target.value)} />
+                </div>
+                <div>
+                    <input type="button" value={"7"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"8"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"9"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"*"} className="operator" onClick={e => setValue(value + e.target.value)} />
+                </div>
+                <div>
+                    <input type="button" value={"4"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"5"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"6"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"-"} className="operator" onClick={e => setValue(value + e.target.value)} />
+                </div>
+                <div>
+                    <input type="button" value={"1"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"2"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"3"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"+"} className="operator" onClick={e => setValue(value + e.target.value)} />
+                </div>
+                <div>
+                    <input type="button" value={"00"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"0"} onClick={e => setValue(value + e.target.value)} />
+                    <input type="button" value={"="} className="spl operator" onClick={e => setValue(eval(value))}/>
+                </div>
+            </form>
+        </div>
+    </div>
+    )
+}
+
+export default Display;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import Display from "./Display.js"
+import "./style.css"
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+    <Display />
+)
